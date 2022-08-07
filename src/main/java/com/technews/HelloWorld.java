e580b301fef9c88d85e74ab5220a0897fa76cb83
ajmarrocco <ajmarrocco@gmail.com>
2022-08-06 22:44:34 -0400

    system.out hello world

--- src/main/java/com/technews/HelloWorld.java
+++ src/main/java/com/technews/HelloWorld.java
@@ -1,7 +1,8 @@
 package com.technews;
 
 public class HelloWorld {
+    // at least one class must have a main() method
     public static void main(String[] args){
-
+        System.out.println("Hello, World!");
     }
 }
