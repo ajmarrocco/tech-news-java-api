603a8f01bc8767c6ad4f0971ab73cc03705b1292
ajmarrocco <ajmarrocco@gmail.com>
2022-08-06 23:08:28 -0400

    create Demo class and add constructor function

--- src/main/java/com/technews/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.technews;
-
-public class HelloWorld {
-    // at least one class must have a main() method
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
-    }
-}
