70500851d753dac44f927eb08120ab77f382327b
ajmarrocco <ajmarrocco@gmail.com>
2022-08-06 23:18:01 -0400

    add constructor

--- src/main/java/com/technews/testModel/Demo.java
+++ src/main/java/com/technews/testModel/Demo.java
@@ -1,9 +1,9 @@
 package com.technews.testModel;
-
+// name of class
 public class Demo {
     private String name;
     private int age;
-
+    // equivalent of constructor in JS
     public Demo(String name, int age) {
         this.name = name;
         this.age = age;
