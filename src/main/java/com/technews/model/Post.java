906715aa905d12816aceb9882bb2b1dfe80392d9
ajmarrocco <ajmarrocco@gmail.com>
2022-08-08 16:06:29 -0400

    add variables

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -1,4 +1,15 @@
 package com.technews.model;
 
-public class Post {
+import java.io.Serializable;
+
+public class Post implements Serializable {
+    private Integer id;
+    private String title;
+    private String posturl;
+    private String username;
+    private int voteCount;
+    private Integer userId;
+    private Date postedAt = new Date();
+    private Date updatedAt = new Date();
+    private List<Comment> comments;
 }
