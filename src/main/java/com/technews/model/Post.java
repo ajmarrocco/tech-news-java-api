3c82f0955394c980a506088fe41001bb25af0905
ajmarrocco <ajmarrocco@gmail.com>
2022-08-08 16:12:22 -0400

    add annotations before class

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -1,6 +1,18 @@
 package com.technews.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
+
+// marks Post class as persistable object so it can map to a table
+@Entity
+// Specifies properties that should be ignored when serializing this object to JSON
+// arguments are the properties that should be ignored
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+// specifies name of table the class maps too
+@Table(name = "post")
 
 public class Post implements Serializable {
     private Integer id;
@@ -12,4 +24,7 @@ public class Post implements Serializable {
     private Date postedAt = new Date();
     private Date updatedAt = new Date();
     private List<Comment> comments;
+
+
+
 }
