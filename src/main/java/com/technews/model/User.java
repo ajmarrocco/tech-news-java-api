fa1c4db5f6f5ec69beb5d79117b94fc6d609134b
ajmarrocco <ajmarrocco@gmail.com>
2022-08-07 23:36:05 -0400

    add variables to user class

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -4,6 +4,8 @@ package com.technews.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.xml.stream.events.Comment;
+import java.util.List;
 
 // marks User class as persistable object so it can map to a table
 @Entity
@@ -13,4 +15,14 @@ import javax.persistence.Table;
 // specifies name of table the class maps too
 @Table(name = "user")
 public class User {
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    boolean loggedIn;
+
+    private List<Post> posts;
+    private List<Vote> votes;
+    private List<Comment> comments;
+
 }
