32c4134557cb562d2c904cff4eb519d07c0385ab
ajmarrocco <ajmarrocco@gmail.com>
2022-08-09 15:20:13 -0400

    add getter and setters to vote model

--- src/main/java/com/technews/model/Vote.java
+++ src/main/java/com/technews/model/Vote.java
@@ -29,4 +29,28 @@ public class Vote implements Serializable {
         this.userId = userId;
         this.postId = postId;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
 }
