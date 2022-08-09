d599e802e674e2634bb4a6278c7d2a2d137526b8
ajmarrocco <ajmarrocco@gmail.com>
2022-08-09 14:53:13 -0400

    add class annotations

--- src/main/java/com/technews/model/Comment.java
+++ src/main/java/com/technews/model/Comment.java
@@ -1,6 +1,23 @@
 package com.technews.model;
 
-public class Comment {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+// marks Comment class as persistable object so it can map to a table
+@Entity
+// Specifies properties that should be ignored when serializing this object to JSON
+// arguments are the properties that should be ignored
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+// specifies name of table the class maps too
+@Table(name = "comment")
+
+public class Comment implements Serializable {
+    // will be unique identifier
+    @Id
+    // will be a generated value
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String commentText;
     private Integer userId;
