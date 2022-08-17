65f63721dd52f1cf69e909994234fa66ef5528ed
ajmarrocco <ajmarrocco@gmail.com>
2022-08-17 11:51:24 -0400

    add findAllCommentsByPostId method to CommentRepository

--- src/main/java/com/technews/repository/CommentRepository.java
+++ src/main/java/com/technews/repository/CommentRepository.java
@@ -1,4 +1,12 @@
 package com.technews.repository;
 
-public interface CommentRepository {
+import com.technews.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findAllCommentsByPostId(int postId);
 }
