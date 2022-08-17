00097ff07a7af5b3959c15e4dd62658124dc49ad
ajmarrocco <ajmarrocco@gmail.com>
2022-08-17 11:57:04 -0400

    create VoteRepository

---
+++ src/main/java/com/technews/repository/VoteRepository.java
@@ -0,0 +1,8 @@
+package com.technews.repository;
+
+import com.technews.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<Vote, Integer> {
+
+}
