01f9881e38036d37a939877712028e69d8a66f8b
ajmarrocco <ajmarrocco@gmail.com>
2022-08-27 22:08:49 -0400

    create getAllUsers method

--- src/main/java/com/technews/controller/UserController.java
+++ src/main/java/com/technews/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.technews.controller;
 
+import com.technews.model.Post;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -12,4 +16,17 @@ public class UserController {
     UserRepository repository;
     @Autowired
     VoteRepository voteRepository;
+
+    @GetMapping("/api/users")
+    public List<User> getAllUsers() {
+        List<User> userList = repository.findAll();
+        for (User u : userList) {
+            List<Post> postList = u.getPosts();
+            for (Post p : postList) {
+                p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+            }
+        }
+        return userList;
+    }
+
 }
