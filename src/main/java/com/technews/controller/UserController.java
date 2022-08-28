e084a76645cd9d26be3884d16a503d567824289c
ajmarrocco <ajmarrocco@gmail.com>
2022-08-27 22:05:05 -0400

    add autowired notation

--- src/main/java/com/technews/controller/UserController.java
+++ src/main/java/com/technews/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.technews.controller;
 
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    // Autowired is similar to "new" except it only instantiates objects as needed
+    @Autowired
+    UserRepository repository;
+    @Autowired
+    VoteRepository voteRepository;
 }
