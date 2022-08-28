25339f5954644b91f83850474dece4c65cf52818
ajmarrocco <ajmarrocco@gmail.com>
2022-08-27 21:45:51 -0400

    check if API is running and db and tables are connected

--- src/main/java/com/technews/TechNewsJavaApiApplication.java
+++ src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TechNewsJavaApiApplication {
 
 	public static void main(String[] args) {
+		// Create automatically using SpringInitialzr to run API
 		SpringApplication.run(TechNewsJavaApiApplication.class, args);
 	}
 
